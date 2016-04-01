06f299a56f04d5b412d6c1becd589de582992ffe
sol1390 <solange.j.spitale@gmail.com>
2016-04-01 10:46:17 -0300

    primer parte

--- Practicas/Spitale/IUA-SO-Practico-1-procesos.c
+++ Practicas/Spitale/IUA-SO-Practico-1-procesos.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <sys/types.h>
 
 int main() {
 	int pid;
-	...
+
 	pid = fork();
+	
 	if (pid == 0) {
 		/* hijo */
-		...
+		printf("\nEstoy en el Proceso hijo. Valor devuelto (%d)\n", pid);
 	} else {
-	/* padre */
-		...
+		/* padre */
+		printf("\nEstoy en el Proceso padre. Su PID es: %d \nEl PID del proceso hijo es: %d\n",getpid(), pid);
 	}
+
 	return 0;
-	}
+}
